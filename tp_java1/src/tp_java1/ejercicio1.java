ca417dd6c3fe7dc95143376f100d8fa31ca9cce7
sebastianp213 <167637762+sebastianp213@users.noreply.github.com>
2025-06-09 13:56:53 -0300

    Add files via upload
    commiteo el primer ejercicio

---
+++ tp_java1/src/tp_java1/ejercicio1.java
@@ -0,0 +1,17 @@
+package tp_java1;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i <= 100 ; i++) {
+			if(i % 2 == 0) {
+				System.out.println("par");
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
